82e30912b567e208b5fa2ea9f4d6d198aab9f634
Eugene <zuevevgen93@mail.ru>
2024-07-31 17:27:12 +0300

    Favicon. CopyPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,7 +4,9 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/types';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
-import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import path from 'path';
+import CopyPlugin from 'copy-webpack-plugin';
 
 // TODO: Деструктурировать можно как таким образом как здесь, так и явным const {mode, paths} = options; как в buildWebpack.ts
 export function buildPlugins({ mode, paths, analyzer, platform }: BuildOptions): Configuration['plugins'] {
@@ -12,7 +14,7 @@ export function buildPlugins({ mode, paths, analyzer, platform }: BuildOptions):
   const isProd = mode === 'production';
 
   const plugins: Configuration['plugins']  = [
-    new HtmlWebpackPlugin({ template: paths.html }),
+    new HtmlWebpackPlugin({ template: paths.html, favicon: path.resolve(paths.public, 'favicon.ico') }),
     new DefinePlugin({
       __PLATFORM__: JSON.stringify(platform),
       __ENV__: JSON.stringify(mode),
@@ -30,6 +32,14 @@ export function buildPlugins({ mode, paths, analyzer, platform }: BuildOptions):
     plugins.push(new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css'
+    }));
+    plugins.push(new CopyPlugin({
+      patterns: [
+        { 
+          from: path.resolve(paths.public, 'locales'), 
+          to: path.resolve(paths.output, 'locales') 
+        },
+      ],
     }));
   }
 
